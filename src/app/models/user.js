6139581f10cbaa4f8b99f0f72bd9e3ffce5ecbea
Wellmtb <wellington.serafim@outlook.com>
2019-09-01 18:36:35 -0300

    Chore: Criando model user.js

---
+++ src/app/models/user.js
@@ -0,0 +1,17 @@
+import sequelize, { model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: sequelize.STRING,
+        email: sequelize.STRING,
+        password_hash: sequelize.STRING,
+        provider: sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
